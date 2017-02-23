d2516fe396116b84e94ab13b9656986b86ab39da
蒼時弦也 <elct9620@frost.tw>
2017-02-23 09:50:17 +0800

    Fix keyboard event move sprite feature

--- examples/hiro.rb
+++ examples/hiro.rb
@@ -2,21 +2,22 @@ class HiroGame < Game
   def initialize
     super
     @sprite = Sprite.new "examples/gamepad.png", Vector2.new(100, 100)
+
+    Event.on Event::KEYDOWN do |data|
+      Hiro.quit! if data.key == Keyboard::ESC
+      # TODO: Implement event manager
+      case data.key
+      when Keyboard::LEFT then @sprite.x -= 10
+      when Keyboard::RIGHT then @sprite.x += 10
+      when Keyboard::UP then @sprite.y -= 10
+      when Keyboard::DOWN then @sprite.y += 10
+      end
+    end
   end
 
   def update
     @sprite.draw
   end
-end
-
-Event.on Event::KEYDOWN do |data|
-  Hiro.quit! if data.key == Keyboard::ESC
-  # case data.key
-  # when Keyboard::LEFT then sprite.x -= 10
-  # when Keyboard::RIGHT then sprite.x += 10
-  # when Keyboard::UP then sprite.y -= 10
-  # when Keyboard::DOWN then sprite.y += 10
-  # end
 end
 
 Event.on Event::QUIT do
