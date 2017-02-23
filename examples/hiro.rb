b18433a392b6ac17ad3f4321ae3246bb3ef0fb1c
蒼時弦也 <elct9620@frost.tw>
2017-02-24 00:38:23 +0800

    Adjust keydown event handler for HiroGame

--- examples/hiro.rb
+++ examples/hiro.rb
@@ -3,20 +3,24 @@ class HiroGame < Game
     super
     @sprite = Sprite.new "examples/gamepad.png", Vector2.new(100, 100)
 
-    Event.on Event::KEYDOWN do |data|
+    # NOTE: Event manager seems can have more improve
+    Event.on Event::KEYDOWN, &on_keydown
+  end
+
+  def update
+    @sprite.draw
+  end
+
+  def on_keydown
+    Proc.new { |data|
       Hiro.quit! if data.key == Keyboard::ESC
-      # TODO: Implement event manager
       case data.key
       when Keyboard::LEFT then @sprite.x -= 10
       when Keyboard::RIGHT then @sprite.x += 10
       when Keyboard::UP then @sprite.y -= 10
       when Keyboard::DOWN then @sprite.y += 10
       end
-    end
-  end
-
-  def update
-    @sprite.draw
+    }
   end
 end
 
