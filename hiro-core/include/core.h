b4e378fc8f4540909257b926ff24df033abeb963
蒼時弦也 <elct9620@frost.tw>
2017-03-21 10:04:03 +0800

    Add basic hiro-core features

--- hiro-core/include/core.h
+++ hiro-core/include/core.h
@@ -3,6 +3,12 @@
 
 #include <mruby.h>
 #include <mruby/class.h>
+#include <mruby/variable.h>
+
+// TODO: Add support for Windows
+#define HIRO_API extern
+
+HIRO_API void hiro_set_instance(mrb_state*, mrb_value);
 
 mrb_value hiro_mrb_set_game_class(mrb_state*, mrb_value);
 mrb_value hiro_mrb_quit_bang(mrb_state*, mrb_value);
--- hiro-core/src/core.c
+++ hiro-core/src/core.c
@@ -1,12 +1,37 @@
 #include "core.h"
 
+void hiro_set_instance(mrb_state* mrb, mrb_value instance) {
+  struct RClass* hiro;
+  mrb_value game_class;
+
+  hiro = mrb_class_get(mrb, "Hiro");
+  game_class = mrb_mod_cv_get(mrb, hiro, mrb_intern_lit(mrb, "game_class"));
+
+  if(!mrb_obj_is_instance_of(mrb, instance, mrb_class_ptr(game_class))) {
+    mrb_raisef(mrb, E_TYPE_ERROR, "Expect %S, but got %S", game_class, instance);
+  }
+
+  mrb_mod_cv_set(mrb, hiro, mrb_intern_lit(mrb, "instance"), instance);
+}
+
 mrb_value hiro_mrb_set_game_class(mrb_state* mrb, mrb_value self) {
-  // TODO: Save user defined game class
+  mrb_value _class;
+
+  // TODO: Check class type is a "Game" instance
+  mrb_get_args(mrb, "C", &_class);
+  mrb_cv_set(mrb, self, mrb_intern_lit(mrb, "game_class"), _class);
+
   return self;
 }
 
 mrb_value hiro_mrb_quit_bang(mrb_state* mrb, mrb_value self) {
-  // TODO: Call "stop!" for game class instance
+  mrb_value instance;
+
+  instance = mrb_cv_get(mrb, self, mrb_intern_lit(mrb, "instance"));
+  if(!mrb_nil_p(instance)) {
+    mrb_funcall(mrb, instance, "stop!", 0);
+  }
+
   return self;
 }
 
