221b6c23a43440acfd331c6b8c96838d80cad823
蒼時弦也 <elct9620@frost.tw>
2017-03-28 21:21:07 +0800

    Add position for game object

--- mrblib/game_object.rb
+++ mrblib/game_object.rb
@@ -1,8 +1,12 @@
 # Game Object
 class GameObject
+  attr_accessor :position
+
   def initialize
+    @parent = nil
     @children = []
     @components = []
+    @position = Vector2.new(0, 0)
   end
 
   def add(child)
@@ -13,6 +17,22 @@ class GameObject
   def component(component)
     return unless component.is_a?(Component)
     @components.push(component)
+  end
+
+  def x
+    @position.x
+  end
+
+  def x=(x)
+    @position.x = x
+  end
+
+  def y
+    @position.y
+  end
+
+  def y=(y)
+    @position.y = y
   end
 
   def update(ticks); end
